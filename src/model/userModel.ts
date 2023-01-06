ac68917b2d09ee5fca2fee0e340ae9d93a7408ff
PierreD59 <pierre.dmt59@gmail.com>
2023-01-06 11:16:58 +0100

    Initial commit et Initialisation du projet. Installation de toute les dépendances.

---
+++ src/model/userModel.ts
@@ -0,0 +1,13 @@
+class User {
+  nom: string;
+  prenom: string;
+  date_de_naissance: string;
+  date_inscription: Date;
+  nationalite?: string;
+
+/** A partir de cette classe, définir une API permetant de gérer nos users
+ *  Contraintes : 
+ *  La date de naissance et la nationnalité sont facultatives
+ *  la date d'inscription doit être au format d/M/Y hh:mm:ss
+ */
+}

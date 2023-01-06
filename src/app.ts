c0e801317116bbec9929cf8f24aa6c0353f0076a
PierreD59 <pierre.dmt59@gmail.com>
2023-01-06 11:27:55 +0100

    Ajout des commandes pour commencer à utiliser express et création du dossier router pour créer une route par défaut

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import UserRouter from "./router/user.router";
+
+// Instance de express
+const app = express();
+// Utilisation d'une variable d'environnement
+const port = process.env.PORT;
+// Pemet de lire le JSON
+app.use(express.json);
+// Instance du router de express ayant pour route par défaut user
+app.use("/user", UserRouter);
+
+// Définition du port pour le serveur
+app.listen(port, () => {
+    console.log(`server lancé sur le port ${port}`);
+  });
